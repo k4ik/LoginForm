a96751e47c49791d5ea5ef5a20c9390d854757bb
k4ik <kaikleal1234@gmail.com>
2024-03-04 17:09:57 -0300

    feat: create 404 page

--- index.php
+++ index.php
@@ -8,5 +8,7 @@ $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if (array_key_exists($url, $routes)) {
     include $routes[$url];
+} else {
+    include './views/404.php';
 }
 ?>
---
+++ views/404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="shortcut icon" href="../assets/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/styles/reset.css">
+    <link rel="stylesheet" href="../assets/styles/404.css">
+</head>
+<body>
+    <div class="container">
+        <h1>404</h1>
+        <p>Oops...page not found!</p>
+        <img src="../assets/images/404_page-not-found.png" alt="">
+    </div>
+</body>
+</html>
