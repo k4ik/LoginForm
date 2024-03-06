e82158005d17ffc2f123f8f9ce6f4cb62c838c76
k4ik <kaikleal1234@gmail.com>
2024-03-06 15:54:32 -0300

    feat: db

--- db/conn.php
+++ db/conn.php
@@ -1,14 +1,18 @@
 <?php 
-    $host = "localhost";
-    $username = "root";
-    $password = "admin";
-    $db = "login";
+    $host = "flora.db.elephantsql.com";
+    $user = "yxrzeear";
+    $pass = "8oaJBR980pz5jOiSxg9zgL7_ViV9wfN1";
+    $db = "yxrzeear";
 
-    $conn = new mysqli($host, $username, $password, $db);
+    // Open a PostgreSQL connection
+    $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+    or die ("Could not connect to server\n");
 
-    if($conn->connect_error) {
-        die("Error" . $conn->connect_error);
-    }
+    $query = "SELECT * FROM users";
+    $results = pg_query($con, $query) or die('Query failed: ' . pg_last_error());
 
-    $conn->close();
+    //$row = pg_fetch_row($results);
+    //echo $row[0] . "\n";
+    // Closing connection
+    pg_close($con);
 ?>
