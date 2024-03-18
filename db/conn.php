0be8da53fe08a1a24c4f2540086469395965114e
k4ik <kaikleal1234@gmail.com>
2024-03-17 21:01:40 -0300

    fix: db conn

--- db/conn.php
+++ db/conn.php
@@ -4,14 +4,6 @@
     $pass = "8oaJBR980pz5jOiSxg9zgL7_ViV9wfN1";
     $db = "yxrzeear";
 
-    // Open a PostgreSQL connection
     $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
     or die ("Could not connect to server\n");
-
-    $query = "SELECT * FROM users";
-    $results = pg_query($con, $query) or die('Query failed: ' . pg_last_error());
-
-    //$row = pg_fetch_row($results);
-    //echo $row[0] . "\n";
-    // Closing connection
 ?>
