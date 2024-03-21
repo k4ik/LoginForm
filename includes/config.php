4cca3e2a9a8a45cd95b5da1d98dce591aedaf569
k4ik <kaikleal1234@gmail.com>
2024-03-20 21:31:59 -0300

    feat: install PHPMailer

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php 
+    define('SMTP_HOST', "smtp.gmail.com");
+    define('SMTP_PORT', "465");
+
+    define('SMTP_USERNAME', "kaik.e.l.couto@gmail.com");
+    define('SMTP_PASSWORD', "hqmcfomlwrnchccy");
+?>
