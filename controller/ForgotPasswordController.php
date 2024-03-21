411e1e448a1164edbd8c7b364eb32e2f1caf9ec2
k4ik <kaikleal1234@gmail.com>
2024-03-20 21:34:58 -0300

    feat: create PHPMailer

--- controller/ForgotPasswordController.php
+++ controller/ForgotPasswordController.php
@@ -1,48 +1,58 @@
-<?php 
-    include '../db/conn.php';
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+<?php
+include '../db/conn.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = $_REQUEST["email"];
-        $oldPassword = $_REQUEST["oldPassword"];
-        $newPassword = $_REQUEST["newPassword"];
-        $confirmNewPassword = $_REQUEST["confirmNewPassword"];
+require '../includes/config.php';
+require '../vendor/autoload.php';
 
-        if(empty($email) || empty($oldPassword) || empty($newPassword) || empty($confirmNewPassword)) {
-            echo "Fill in the fields!";
-            return;
-        }
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-        $query = "SELECT * FROM users WHERE email='$email' AND password='$oldPassword';";
-        $check_result = pg_query($con, $query);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_REQUEST["email"];
 
-        if(!$check_result) {
-            echo "An error has occurred! Try again!";
-        } else {
-            if(pg_num_rows($check_result) == 1) {
-                if($oldPassword == $newPassword) {
-                    echo "The new password must be different from the current one";
-                    return;
-                }
-
-                if($newPassword != $confirmNewPassword) {
-                    echo "Passwords do not match";
-                    return;
-                } 
-
-                $query2 = "UPDATE users SET password='$newPassword' WHERE email='$email';";
-                $check_result2 = pg_query($con, $query2);
-                if(!$check_result2) {
-                    echo "An error has occurred! Try again!";
-                } else {
-                    echo "Password changed successfully";
-                }
-            } else {
-                echo "User not found";
-            }
-        }
-    
+    if (empty($email)) {
+        echo "Fill in the fields!";
+        return;
     }
-?>
+
+    $query = "SELECT * FROM users WHERE email='$email';";
+    $check_result = pg_query($con, $query);
+
+    if (pg_num_rows($check_result) == 1) {
+        $mail = new PHPMailer(true);
+        $verificationCode = rand();
+
+        $query2 = "UPDATE users SET verification_code='$verificationCode' WHERE email='$email';";
+        pg_query($con, $query2);
+
+        try {
+            $mail->isSMTP();
+            $mail->Host       = SMTP_HOST;
+            $mail->SMTPAuth   = true;
+            $mail->Username   = SMTP_USERNAME;
+            $mail->Password   = SMTP_PASSWORD;
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port       = SMTP_PORT;
+
+            $mail->setFrom("kaik.e.l.couto@gmail.com", "Equipe de Suporte");
+            $mail->addAddress($email);
+            $mail->isHTML(true);
+            $mail->CharSet = 'UTF-8';
+            $mail->Subject = "=?UTF-8?B?" . base64_encode("Redefinição de Senha - Código de Verificação: $verificationCode") . "?=";
+            $mail->Body = "<p>Para redefinir sua senha, acesse o link abaixo e insira o código de verificação fornecido:</p>";
+            $mail->Body .= "<p>Link: http://localhost:8000/verification-code</p>";
+            $mail->Body .= "<p>Código de verificação: $verificationCode</p><br>";
+            $mail->Body .= "<p>Atenciosamente,<br>Equipe de Suporte.</p>";
+
+            $mail->send();
+        } catch (Exception $e) {
+            die("Erro");
+        }
+    } else {
+        echo "User not found! Try Again.";
+    }
+}
