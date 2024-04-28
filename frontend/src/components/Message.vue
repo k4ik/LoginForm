ced5e18fbadbc84c27a27c31df117b0f883ae826
k4ik <kaikleal1234@gmail.com>
2024-04-28 15:16:30 -0300

    feat: create message component

---
+++ frontend/src/components/Message.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="message">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["message"],
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/_message.scss";
+</style>    
